7e65e57d25a93bfd9edc256563f9e3ac56338afd
Samantha King <samanthak2010@live.com>
2022-03-26 16:57:32 -0500

    CAC-103 Lesson 3 Operators

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -21,7 +21,28 @@ inside that member.
 // Lesson1Example();
 // Exercise1Example();
 // Lesson2Example();
-Exercise2Example();
+// Exercise2Example();
+Lesson3Example();
+
+
+// Lesson 3 Operators and Overflow Checking
+void Lesson3Example()
+{
+    Lesson3 myLesson3 = new Lesson3();
+    // myLesson3.BasicCheckedOperator(14);
+    // myLesson3.BasicCheckedExample2();
+    // int alpha = 2147483647;
+    // int total = alpha + 10;
+    // Console.WriteLine(total);
+    // -2147483639
+    // myLesson3.BasicUncheckedExample();
+    // myLesson3.BasicMath();
+    // myLesson3.BasicModulus(783);
+    // myLesson3.BasicBooleanLogical(true, false);
+    // myLesson3.BasicCompoundExample(12, 35, 9);
+    // myLesson3.BasicRelationalExample("Tom", "Tim");
+    myLesson3.MyIncDecrExample();
+}
 
 
 // Lesson 2 Lab Exercise
