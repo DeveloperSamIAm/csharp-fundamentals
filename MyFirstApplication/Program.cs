fa583b0998d2f9ef9217afcdc6e9ba18456e040a
Samantha King <samanthak2010@live.com>
2022-04-09 13:30:02 -0500

    CAC-103 Lesson 7 Records & Structs

--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -20,7 +20,32 @@
 // Lesson5Example();
 // Exercise5Example();
 // Lesson6Examples();
-Exercise6Example();
+// Exercise6Example();
+Lesson7Example();
+
+
+// Lesson 7
+void Lesson7Example()
+{
+    Location myLocation = new Location(34.5, 47.8);
+    Console.WriteLine(myLocation.Latitude);
+    Console.WriteLine(myLocation);
+
+    // Readonly Struct
+    Student myStudent = new Student(10, "Roger");
+    Console.WriteLine(myStudent);
+    Console.WriteLine(myStudent.StudentName);
+
+    // Record
+    Person person = new Person("Nancy", "Drew");
+    Console.WriteLine(person);
+    Console.WriteLine(person.lastName);
+
+    // Struct Record
+    Resolution res = new Resolution(1920, 1080);
+    res.CalculateRes();
+    Console.WriteLine(res);
+}
 
 
 // Lesson 6 Lab Exercise
